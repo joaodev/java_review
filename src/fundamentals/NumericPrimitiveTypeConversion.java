3da820d6a3086b23b2c84d17117a6a548b477b06
João Augusto Bonfante <joao.dev93@gmail.com>
2026-01-16 13:21:10 -0300

    add: implement various Java programs for console input, number conversion, and primitive type handling

---
+++ src/fundamentals/NumericPrimitiveTypeConversion.java
@@ -0,0 +1,21 @@
+package fundamentals;
+
+public class NumericPrimitiveTypeConversion {
+
+    public static void main(String[] args) {
+
+        double a = 1; // IMPLICIT CAST
+        System.out.println(a);
+
+        float b = (float) 1.1234567; // EXPLICIT CAST
+        System.out.println(b);
+
+        int c = 340;
+        byte d = (byte) c; // EXPLICIT CAST
+        System.out.println(d);
+
+        double e = 1.9999999;
+        int f = (int) e; // EXPLICIT CAST
+        System.out.println(f);
+    }
+}
