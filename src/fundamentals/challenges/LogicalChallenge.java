453091fd8a8c2f4ad22222e8c948bfbdf97414d3
João Augusto Bonfante <joao.dev93@gmail.com>
2026-01-20 15:51:30 -0300

    add: implement examples of assignment, logical, relational, ternary, and unary operators in new Java classes

---
+++ src/fundamentals/challenges/LogicalChallenge.java
@@ -0,0 +1,27 @@
+package fundamentals.challenges;
+
+public class LogicalChallenge {
+
+    static void main(String[] args) {
+
+        // Works on Tuesday (V or F)
+        // Works on Thursday (V or F)
+
+        boolean work1 = true;
+        boolean work2 = false;
+
+        // Binary operator
+        boolean buyTV50 = work1 && work2;
+        boolean buyTV32 = work1 ^ work2;
+        boolean buyIceCream = work1 || work2;
+
+        // Unary operator
+        boolean moreHealthy = !buyIceCream;
+
+        System.out.println("Buy TV 50\"? " + buyTV50);
+        System.out.println("Buy TV 32\"? " + buyTV32);
+
+        System.out.println("Buy ice cream? " + buyIceCream);
+        System.out.println("More healthy? " + moreHealthy);
+    }
+}
---
+++ src/fundamentals/operators/TernaryOperators.java
@@ -0,0 +1,20 @@
+package fundamentals.operators;
+
+public class TernaryOperators {
+
+    static void main(String[] args) {
+
+        double studentGrade = 7.6;
+        String partialResult = studentGrade >= 5.0 ? "recovery" : "disapproved";
+        String finalResult = studentGrade >= 7.0 ? "approved": partialResult;
+        System.out.println("The student was " + finalResult + ".");
+
+        double grade = 7.3;
+        boolean goodBehavior = true;
+        boolean passedByGrade = grade >= 7;
+        boolean haveDiscount = goodBehavior && passedByGrade;
+
+        String result = haveDiscount ? "Yes" : "No";
+        System.out.printf("Have discount? %s%n", result);
+    }
+}
